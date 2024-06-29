4833c54646110c538a19c4322599d814fbce6e58
PC <PC@DESKTOP-J6EDVLT>
2024-06-29 12:33:36 +0300

    Description

---
+++ Infrastructure/OnionAPI.presentetion/Registration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnionAPI.presentetion.Context;
+
+
+namespace OnionAPI.presentetion
+{
+    public static class Registration
+    {
+        public static void AddPersistance(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
